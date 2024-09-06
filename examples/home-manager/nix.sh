bdd1caa246fc49dcf150ea5ffb6cdb20cfb391fc
Adonis <adopi.naj@gmail.com>
2024-09-06 23:39:23 +0200

    Update nix.sh

--- examples/home-manager/nix.sh
+++ examples/home-manager/nix.sh
@@ -1,5 +1,1 @@
-# sudo nixos-rebuild switch --flake .#osaka
-# sudo nix-env --delete-generations old --profile /nix/var/nix/profiles/system
-#
-#
 sudo nixos-rebuild switch --flake .#myHome --show-trace
