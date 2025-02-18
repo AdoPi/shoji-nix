f906e3f9364938099d72b990e4845c12acb7ead0
Adonis <adopi.naj@gmail.com>
2025-02-18 09:48:26 +0100

    use v0.0.3
    Fix case sensitive and ignoring Include option in ssh config file.

--- pkgs/shoji.nix
+++ pkgs/shoji.nix
@@ -7,16 +7,16 @@
 buildGoModule rec {
   pname = "shoji";
   name = "shoji";
-  version = "0.0.2";
+  version = "0.0.3";
 
   src = fetchFromGitHub {
     owner = "AdoPi";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-5g0GaUfdndTfPMYQeNPR2mYlc6Y7YoGlVNBodmK3uiY=";
+    hash = "sha256-vylraBVsuGFEFgd3GfKVWiMh2yQ2SYYURNidAJKSaus=";
   };
 
-  vendorHash = "sha256-uvpMGk0MbjR7kGRL2K1uP1vH30TAuz/ULEjObW6udyA=";
+  vendorHash = "sha256-vylraBVsuGFEFgd3GfKVWiMh2yQ2SYYURNidAJKSaus=";
 
   meta = with lib; {
     description = "SSH Key Management Module";
