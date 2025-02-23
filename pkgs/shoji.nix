140edf62db7af9640361be0c0a492783b8d550ca
Adonis <adopi.naj@gmail.com>
2025-02-23 13:15:12 +0100

    use shoji v0.0.6
    Ignore "Include" option

--- pkgs/shoji.nix
+++ pkgs/shoji.nix
@@ -7,13 +7,13 @@
 buildGoModule rec {
   pname = "shoji";
   name = "shoji";
-  version = "0.0.3";
+  version = "0.0.6";
 
   src = fetchFromGitHub {
     owner = "AdoPi";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-vylraBVsuGFEFgd3GfKVWiMh2yQ2SYYURNidAJKSaus=";
+    hash = "sha256-gh3612yuOBySgA55ID2clQ5KWmtMbTusx9UTOuhgAmc=";
   };
 
   vendorHash = "sha256-uvpMGk0MbjR7kGRL2K1uP1vH30TAuz/ULEjObW6udyA=";
